0ab6b48ee7e0478f56ece9c9e7440fe05239ecfc
Andrew Lum <hello@andlum.com>
2024-01-05 16:04:24 -0500

    finished interviewing

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,53 @@
 import figlet from "figlet";
 import readline from "readline";
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+export const main = (): void => {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
 
-rl.question("What is your name? ", (name: string) => {
-  console.log(`Hello ${name}!`);
-  rl.close();
-});
+  const word = "earth";
+
+  rl.question("What is your guess? ", (guess: string) => {
+    const result = new Array(5).fill("⬜");
+    const formattedGuess = guess.toLowerCase();
+    let available = [...word];
+
+    // for (let i = 0; i < word.length; i++) {
+    //   const letter = formattedGuess.at(i) || "";
+    //   if (letter === word[i]) {
+    //     result.push("🟩");
+    //     available[i] = "";
+    //   } else if (available.indexOf(letter) > -1) {
+    //     result.push("🟨");
+    //     available[available.indexOf(letter)] = "";
+    //   } else {
+    //     result.push("⬜");
+    //   }
+    // }
+
+    for (let i = 0; i < word.length; i++) {
+      const letter = formattedGuess.at(i);
+      if (letter === word[i]) {
+        result[i] = "🟩";
+        available[i] = "";
+      }
+    }
+
+    for (let i = 0; i < word.length; i++) {
+      const letter = formattedGuess.at(i) || "";
+
+      if (available.indexOf(letter) > -1) {
+        result[i] = "🟨";
+        available[available.indexOf(letter)] = "";
+      }
+    }
+
+    console.log(result.join(""));
+
+    rl.close();
+  });
+};
+
+main();
