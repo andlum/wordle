c9e21ed0c540f9b3e42d2fa283d179cfb5d9e29d
Andrew Lum <hello@andlum.com>
2024-01-05 17:33:24 -0500

    Refactor so Wordle game has guesses

--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,64 @@
 import figlet from "figlet";
 import readline from "readline";
 
-export const main = (): void => {
-  const rl = readline.createInterface({
+const prompt = (guess: string, answer: string): boolean => {
+  const formattedGuess = guess.toLowerCase().slice(0, 5);
+  const result = new Array(5).fill("⬜");
+
+  if (formattedGuess == answer) {
+    console.log("🟩🟩🟩🟩🟩");
+    return true;
+  }
+
+  let bank = [...answer];
+
+  formattedGuess.split("").map((letter, i) => {
+    if (letter == answer.at(i)) {
+      bank[i] = "";
+      result[i] = "🟩";
+    }
+  });
+
+  formattedGuess.split("").forEach((letter, i) => {
+    if (result[i] == "⬜") {
+      const j = bank.indexOf(letter);
+      if (j > -1) {
+        result[i] = "🟨";
+        bank[j] = "";
+      }
+    }
+  });
+
+  console.log(result.join(""));
+  return false;
+};
+
+export const play = (answer: string): void => {
+  const prompter = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
   });
 
-  const word = "earth";
+  const MAX_GUESSES = 5;
+  let attempts = 0;
+  let success = false;
 
-  rl.question("What is your guess? ", (guess: string) => {
-    const result = new Array(5).fill("⬜");
-    const formattedGuess = guess.toLowerCase();
-    let available = [...word];
+  const fAnswer = answer.toLowerCase();
 
-    // for (let i = 0; i < word.length; i++) {
-    //   const letter = formattedGuess.at(i) || "";
-    //   if (letter === word[i]) {
-    //     result.push("🟩");
-    //     available[i] = "";
-    //   } else if (available.indexOf(letter) > -1) {
-    //     result.push("🟨");
-    //     available[available.indexOf(letter)] = "";
-    //   } else {
-    //     result.push("⬜");
-    //   }
-    // }
+  prompter.setPrompt("Guess a word: ");
 
-    for (let i = 0; i < word.length; i++) {
-      const letter = formattedGuess.at(i);
-      if (letter === word[i]) {
-        result[i] = "🟩";
-        available[i] = "";
-      }
+  prompter.prompt();
+  prompter.on("line", (guess) => {
+    success = prompt(guess, fAnswer);
+    attempts++;
+
+    if (success || attempts >= MAX_GUESSES) {
+      prompter.close();
+    } else {
+      prompter.prompt();
     }
-
-    for (let i = 0; i < word.length; i++) {
-      const letter = formattedGuess.at(i) || "";
-
-      if (available.indexOf(letter) > -1) {
-        result[i] = "🟨";
-        available[available.indexOf(letter)] = "";
-      }
-    }
-
-    console.log(result.join(""));
-
-    rl.close();
   });
 };
 
-main();
+const answer = "earth";
+play(answer);
